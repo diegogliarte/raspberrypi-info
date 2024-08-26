8871aef293017e9f7d50ad995eea2c879eaf372c
Diego González Liarte <63498452+diegogliarte@users.noreply.github.com>
2024-08-26 19:08:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
 from fastapi import FastAPI
 import platform
 import psutil
+import subprocess
 
 app = FastAPI()
 
+def get_cpu_temperature():
+    try:
+        temp_output = subprocess.check_output(["vcgencmd", "measure_temp"]).decode()
+        temp = float(temp_output.split('=')[1].split("'")[0])
+        return temp
+    except Exception as e:
+        return None
+
 def get_system_info():
     return {
-        "status": "running",  # Indicates that the Raspberry Pi is operational
+        "status": "running",
         "system": platform.system(),
         "node": platform.node(),
         "release": platform.release(),
         "cpu_count": psutil.cpu_count(logical=False),
         "cpu_count_logical": psutil.cpu_count(logical=True),
+        "cpu_usage": psutil.cpu_percent(interval=0),
+        "cpu_usage_per_core": psutil.cpu_percent(interval=0, percpu=True),
         "memory_total": psutil.virtual_memory().total,
-        "memory_available": psutil.virtual_memory().available
+        "memory_available": psutil.virtual_memory().available,
+        "temperature": get_cpu_temperature(),
     }
 
 @app.get("/info")
